72f5a6d1d308e33d9f75869a052f2a8209de0856
jonathan rawlings <jonathanrawlings@jonathans-MBP.home>
2021-03-01 11:26:44 -0800

    deployment netlify

--- src/context.js
+++ src/context.js
@@ -1,4 +1,4 @@
-import { Collapse } from 'bootstrap';
+
 import React, { Component } from 'react';
 import { storeProducts, detailProduct } from './data';
 const ProductContext = React.createContext();
